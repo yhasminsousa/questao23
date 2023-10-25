7dc3dc632450905f7142de9eb2e9ae36a72c5c4d
palomalindinha01 <palomalindinha35@gmail.com>
2023-10-25 11:33:02 -0300

    first commit

---
+++ questao23.py
@@ -0,0 +1,5 @@
+# Faça um programa que calcule a soma entre todos os números que são múltiplos de três
+# e que se encontram no intervalo de 1 até 500.
+for multiplos in range (1,500,):
+    if (multiplos % 3 == 0):
+        print (multiplos)
